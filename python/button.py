f5f6a92fd6b97e1f63243ca787b970092ffb7cc5
Sylvian Kimkpe <yhawkhisses@gmail.com>
2023-03-13 18:02:32 +0000

    websockets frontend

--- python/button.py
+++ python/button.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 import requests
+import os
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -16,18 +18,14 @@ while True:
     input_state4 = GPIO.input(26)
 
     if input_state1 == False:
-        print('Button1 Pressed')
         requests.post('http://localhost:3000/button1')
         time.sleep(0.2)
     elif input_state2 == False:
-        print('Button2 Pressed')
         requests.post('http://localhost:3000/button2')
         time.sleep(0.2)
     elif input_state3 == False:
-        print('Button3 Pressed')
         requests.post('http://localhost:3000/button3')
         time.sleep(0.2)
     elif input_state4 == False:
-        print('Button4 Pressed')
         requests.post('http://localhost:3000/button4')
         time.sleep(0.2)
